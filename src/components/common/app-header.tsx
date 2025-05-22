f3db44d0635f6b97f621a9355b276699d1370cb3
iamhafa <iamhafaa@gmail.com>
2025-05-22 18:16:20 +0700

    upgrade tailwind from v3 to v4

--- src/components/common/app-header.tsx
+++ src/components/common/app-header.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function AppHeader({ title, sub_title }: Props) {
   return (
-    <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-6 px-8 shadow-md">
+    <header className="bg-linear-to-r from-blue-500 to-indigo-600 text-white py-6 px-8 shadow-md">
       <div className="max-w-4xl mx-auto text-center">
         <h1 className="text-3xl md:text-4xl font-bold">{title}</h1>
         {sub_title && <h2 className="text-lg md:text-xl font-light mt-2 opacity-90">{sub_title}</h2>}
